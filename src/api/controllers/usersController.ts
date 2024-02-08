3a0abbe9e0c191c89d15e956af020869d0c431fb
Salaheldin Mohamed <75949002+salah-mo@users.noreply.github.com>
2024-02-08 17:23:04 +0200

    Add user controller

---
+++ src/api/controllers/usersController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import Item from "../../db/models/users";
+
+//Get all users
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await Item.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+//Get a single user by id
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const user = await Item.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+//Create a new user
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = new Item(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+//Update a user by id
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await Item.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+//Delete a user by id
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    await Item.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
