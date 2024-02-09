96ac50f1f3df6d5bf1a730141e494fa941f78d3e
Salaheldin Mohamed <75949002+salah-mo@users.noreply.github.com>
2024-02-09 02:43:48 +0200

    Update postsRoutes.ts to use postsController

--- src/api/routes/postsRoutes.ts
+++ src/api/routes/postsRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import * as usersController from "../controllers/postsController";
+import * as postsController from "../controllers/postsController";
 
 const router = express.Router();
 
-router.get("/", usersController.getPosts);
-router.get("/:id", usersController.getPost);
-router.post("/", usersController.createPost);
-router.put("/:id", usersController.updatePost);
-router.delete("/:id", usersController.deletePost);
+router.get("/", postsController.getPosts);
+router.get("/:id", postsController.getPost);
+router.post("/", postsController.createPost);
+router.put("/:id", postsController.updatePost);
+router.delete("/:id", postsController.deletePost);
 
 export default router;
