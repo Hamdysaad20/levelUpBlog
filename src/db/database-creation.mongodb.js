624a7a433fd397f96752b27865cc634d0a0a84ef
Salaheldin Mohamed <75949002+salah-mo@users.noreply.github.com>
2024-02-09 02:39:13 +0200

    Add user and post collections with validation and sample data

--- src/db/database-creation.mongodb.js
+++ src/db/database-creation.mongodb.js
@@ -1,19 +1,26 @@
 use('blogs-db')
-db.createCollection("users", {
-    validator: {
-        $jsonSchema: {
+db.createCollection("users",
+{
+    validator:
+    {
+        $jsonSchema:
+        {
             bsonType: "object",
             required: ["username", "email", "password"],
-            properties: {
-                username: {
+            properties:
+            {
+                username:
+                {
                     bsonType: "string",
                     description: "Username must be a string and is required"
                 },
-                email: {
+                email:
+                {
                     bsonType: "string",
                     description: "Email must be a string and is required"
                 },
-                password: {
+                password:
+                {
                     bsonType: "string",
                     description: "Password must be a string and is required"
                 },
@@ -21,27 +28,52 @@ db.createCollection("users", {
         }
     }
 })
+db.users.insertOne({
+    username: "john_doe",
+    email: "john.doe@example.com",
+    password: "password123"
+})
+
 db.createCollection("posts", {
-    validator: {
-        $jsonSchema: {
+    validator:
+    {
+        $jsonSchema:
+        {
             bsonType: "object",
-            required: ["title", "content", "author", "createdAt"],
-            properties: {
-                title: {
+            required: ["title","image","summary", "content", "author", "createdAt", "keywords"],
+            properties:
+            {
+                title:
+                {
                     bsonType: "string",
                     description: "Title must be a string and is required"
                 },
-                content: {
+                image:
+                {
+                    bsonType: "string",
+                    description: "Image must be a string and is required"
+                },
+                summary:
+                {
+                    bsonType: "string",
+                    description: "Summary must be a string and is required"
+                },
+                content:
+                {
                     bsonType: "array",
-                    items: {
+                    items:
+                    {
                         bsonType: "object",
                         required: ["type", "data"],
-                        properties: {
-                            type: {
+                        properties:
+                        {
+                            type:
+                            {
                                 bsonType: "string",
                                 description: "Type must be a string and is required"
                             },
-                            data: {
+                            data:
+                            {
                                 bsonType: "string",
                                 description: "Data must be a string and is required"
                             }
@@ -49,41 +81,85 @@ db.createCollection("posts", {
                     },
                     description: "Content must be an array of objects and is required"
                 },
-                author: {
+                author:
+                {
                     bsonType: "objectId",
                     description: "Author must be a valid ObjectId and is required"
                 },
                 createdAt: {
                     bsonType: "date",
                     description: "Creation date must be a date and is required"
+                },
+                keywords:
+                {
+                    bsonType: "array",
+                    items:
+                    {
+                        bsonType: "string",
+                        description: "Keywords must be an array of strings"
+                    },
+                    description: "Keywords must be an array and is required"
                 }
             }
         }
     }
 })
-db.createCollection("comments", {
-    validator: {
-        $jsonSchema: {
+db.posts.insertOne({
+    title: "Sample Post",
+    image: "sample.jpg",
+    summary: "This is a sample post",
+    content: [
+        {
+            type: "text",
+            data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+        {
+            type: "image",
+            data: "image.jpg"
+        }
+    ],
+    author: ObjectId("60a6e6e8c8e8e8e8e8e8e8e8"),
+    createdAt: new Date(),
+    keywords: ["sample", "dummy", "data"]
+})
+
+db.createCollection("comments",
+{
+    validator:
+    {
+        $jsonSchema:
+        {
             bsonType: "object",
             required: ["content", "author", "post", "createdAt"],
-            properties: {
-                content: {
+            properties:
+            {
+                content:
+                {
                     bsonType: "string",
                     description: "Content must be a string and is required"
                 },
-                author: {
+                author:
+                {
                     bsonType: "objectId",
                     description: "Author must be a valid ObjectId and is required"
                 },
-                post: {
+                post:
+                {
                     bsonType: "objectId",
                     description: "Post must be a valid ObjectId and is required"
                 },
-                createdAt: {
+                createdAt:
+                {
                     bsonType: "date",
                     description: "Creation date must be a date and is required"
                 },
             }
         }
     }
+})
+db.comments.insertOne({
+    content: "This is a sample comment",
+    author: ObjectId("60a6e6e8c8e8e8e8e8e8e8e8"),
+    post: ObjectId("60a6e6e8c8e8e8e8e8e8e8e8"),
+    createdAt: new Date()
 })
